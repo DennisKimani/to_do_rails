2751586a69fce5884a8107dd8112aa1f6428149c
DennisKimani <denkimoney@gmial.com>
2017-08-02 12:15:11 +0300

    done with the list part of it. one can create read update and delete a list it contains all the CRUD functionalities and all pass successfully.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -22,4 +22,24 @@ class ListsController < ApplicationController
     @list = List.find(:id)
     render :edit
   end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
