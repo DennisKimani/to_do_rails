96aecfeb87c4ab35ed8cf3a37c6f6f46e12bc685
DennisKimani <denkimoney@gmial.com>
2017-08-02 09:50:41 +0300

    added aform to enter a new list.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,4 +3,23 @@ class ListsController < ApplicationController
     @lists = List.all
     render :index
   end
+
+  def new
+    @list = List.all
+    render :new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @list = List.find(:id)
+    render :edit
+  end
 end
